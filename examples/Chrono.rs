6b4c286d74266ecae1ce73f582fab198447e191c
Aashu <Aashupatil218@gmail.com>
2025-11-25 17:53:44 +0530

    package managment

---
+++ examples/Chrono.rs
@@ -0,0 +1,15 @@
+
+use chrono::{Local,Utc};
+
+
+fn main(){
+
+    let now=Local::now();
+    println!("Current date and time: {}", now);
+
+    let  utc=Utc::now();
+
+    let formated=utc.format("%Y-%M-%D %H:%M:%S");
+
+    println!("UTC date and time: {}",formated);
+}
