e5fa6598f0a3da0ddad42824af56f1b17f99768e
Aashu <Aashupatil218@gmail.com>
2025-11-25 18:36:49 +0530

    borrow  of a moved variable

---
+++ examples/Memory.rs
@@ -0,0 +1,24 @@
+
+
+fn create_String(){
+
+
+    let s1=String::from("Hello");
+    // let s2=s1;
+    let s2=s1;
+    // let s1=s2;
+
+    // do_some(s2);
+
+    println!("{}",s1);
+    println!("{}",s2);
+}
+
+// fn do_some(s3:String){
+//     println!("{}",s3);
+// }
+
+fn main(){
+
+    create_String();
+}
