d1fff37accb1938d6d9ca818dec8ff997801662e
Aashu <Aashupatil218@gmail.com>
2025-11-25 12:34:31 +0530

    result enum

---
+++ examples/ResultEnum.rs
@@ -0,0 +1,14 @@
+
+use std::fs::read_to_string;
+
+fn main(){
+    let result=read_to_string("examples/a.txt");
+
+    match result{
+        Ok(data) => println!("File contents:\n{}",data),
+        Err(e) => println!("Error reading file: {}",e),
+    }
+
+
+}
+
