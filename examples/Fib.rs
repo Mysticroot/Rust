4c04ae1ae84dc224398c3de91c1e64fd14299a54
Aashu <Aashupatil218@gmail.com>
2025-11-25 10:48:06 +0530

    fib series

---
+++ examples/Fib.rs
@@ -0,0 +1,26 @@
+
+fn main(){
+
+    println!("{}", fib(10));
+
+}
+
+fn fib(num : i32) ->i32{
+
+    let mut first=0;
+    let mut second=1;
+
+    if num==0 {return first;}
+    else if num==1 {return second;}
+
+    for _ in 1..num {
+        let temp=second;
+        second=first+second;
+        first=temp;
+
+    }
+     second
+
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+
+
+
 fn main() {
     println!("{}", is_even(4));
 }
