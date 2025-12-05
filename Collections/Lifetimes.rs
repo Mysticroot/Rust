1ce19ad413e550dc4686a8db27e62e41ec01d976
Aashu <Aashupatil218@gmail.com>
2025-12-05 11:16:20 +0530

    lifetimes

---
+++ Collections/Lifetimes.rs
@@ -0,0 +1,23 @@
+
+
+
+fn main(){
+
+    let largest;
+    let str1=String::from("small");
+   // {
+    let str2=String::from("longer");
+
+     largest=bigger(&str1,&str2);
+//  } // str2 goes out of scope here, but largest has a reference to it
+     print!("{}",largest);
+}
+
+fn bigger<'a>(a:&'a str,b:&'a str)->&'a str{
+    if a.len() > b.len(){
+        a
+    }
+    else{
+        b
+    }
+}
